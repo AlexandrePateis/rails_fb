d59d10e136f33f8da48cbbffaf788c1ca26e482c
Alexandre Pateis <alepateis9090@gmail.com>
2022-11-24 14:33:33 -0300

    finalizando cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,9 +7,11 @@ class Ability
     user ||= User.new
     return unless user.present?
 
-    can :read, Article
-    can :update, Article, user: user
-    can :destroy, Article, user: user
+
+    can :read, Article, status: 'private', user_id: user.id
+    can :read, Article, status: 'archived', user_id: user.id
+    can :read, Article, status: 'public'
+    can [:update, :destroy], Article, user: user
     can :create, Article
 
 
